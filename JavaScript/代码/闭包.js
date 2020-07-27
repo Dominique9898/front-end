99c84974a551d6c9ac87fc8c084ecc7418a98f4a
Dominique9898 <826931133@qq.com>
2020-07-27 21:05:38 +0800

    .

--- JavaScript/代码/闭包.js
+++ JavaScript/代码/闭包.js
@@ -3,7 +3,7 @@
  * @version: 
  * @Author: Dominique Wei
  * @Date: 2020-03-29 12:17:47
- * @LastEditTime: 2020-07-25 15:21:38
+ * @LastEditTime: 2020-07-27 16:47:38
  */
 
 function User () {
@@ -23,3 +23,4 @@ function User () {
 var p1 = new User()
 p1.login(123, 456)
 console.log(p1.username);
+console.log(new Date().getTime())
