aad36481c5d5b04ade08cf116041cd9c3b9c0343
Dominique9898 <826931133@qq.com>
2020-08-02 00:09:34 +0800

    .

---
+++ JavaScript/代码/链式调用.js
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: Dominique Wei
+ * @Date: 2020-07-31 01:45:07
+ * @LastEditTime: 2020-07-31 17:00:36
+ */ 
+var emotion = {
+    qq_emotion : {
+        a:1,
+        b:2
+    },
+    emoji : {
+        c:2,
+        d:3
+    }
+}
+
+let {...args}  = emotion
+
+console.log(args);
